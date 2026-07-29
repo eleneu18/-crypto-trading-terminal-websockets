5e5555602f44b90200b920a19185ca6ded5d43c4
eleneu18 <elenesanikidze18@gmail.com>
2026-07-29 05:45:30 -0700

    feat: enhance page layout with coin overview and add table components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import Image from "next/image"
 
 const page = () => {
   return (
-    <div className="text-red-500">
-      page
-    </div>
+    <main className="main-container">
+      <section className="home-grid">
+        <div id="coin-overview">
+          <div className="header pt-2">
+            <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt="Bitcoin" width={56} height={56} />
+            
+            <div className="info">
+              <p>Bitcoin / BTC</p>
+              <h1>$123</h1>
+            </div>
+          </div>
+        </div>
+        <p>Trending Coins</p>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
   )
 }
 
