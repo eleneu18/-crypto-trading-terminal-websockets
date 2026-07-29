4ec8f267bd6e6493eb1d37e3c801e918dbc1032b
eleneu18 <elenesanikidze18@gmail.com>
2026-07-29 07:13:23 -0700

    feat: add CoinOverview and TrendingCoins components with loading states

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import CoinOverview from "@/components/home/CoinOverview"
+import TrendingCoins from "@/components/home/TrendingCoins"
+import { Suspense } from "react"
 
 
 const page = async () => {
@@ -5,9 +8,13 @@ const page = async () => {
   return (
     <main className="main-container">
       <section className="home-grid">
-        
-        
+        <Suspense fallback={<div>Loading Overview...</div>}>
+          <CoinOverview />
+        </Suspense>
 
+        <Suspense fallback={<div>Loading Trending Coins...</div>}>
+          <TrendingCoins />
+        </Suspense>
       </section>
 
       <section className="w-full mt-7 space-y-4">
